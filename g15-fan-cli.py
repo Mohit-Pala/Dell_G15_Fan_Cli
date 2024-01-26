b8c478ae085411a3740ee4f9d75fa93ef518d60b
Mohit-Pala <palav@appstate.edu>
2024-01-26 11:20:48 -0500

    added checks for acpi call kernel module

--- g15-fan-cli.py
+++ g15-fan-cli.py
@@ -1,59 +1,111 @@
 import pexpect
 import sys
 
+tmpShell = pexpect.spawn('bash', encoding='utf-8', logfile=sys.stdout)
+tmpShell.expect("[#$]")
+
+
 def parseAgrs():
 
     try:
         # Help menu
         if sys.argv[1].lower().startswith('h'):
             printHelp()
-        
-        # Performance mode
-        # "\_SB.AMW3.WMAX 0 0x15 {0x01, 0xa1, 0x00, 0x00}"
-        if sys.argv[1].lower().startswith('p'):
+
+        elif sys.argv[1].lower().startswith('p'):
             perfMode()
 
-        # Quiet mode
-        # "\_SB.AMW3.WMAX 0 0x15 {0x01, 0xa3, 0x00, 0x00}"
-        if sys.argv[1].lower().startswith('q'):
+        elif sys.argv[1].lower().startswith('q'):
             quietMode()
 
-        # Balanced mode
-        # "\_SB.AMW3.WMAX 0 0x15 {0x01, 0xa0, 0x00, 0x00}"
-        if sys.argv[1].lower().startswith('b'):
+        elif sys.argv[1].lower().startswith('b'):
             balancedMode()
 
-        # Gmode
-        # "\_SB.AMW3.WMAX 0 0x15 {1, 0xab, 0x00, 0x00}"
-        # "\_SB.AMW3.WMAX 0 0x25 {1, 0x01, 0x00, 0x00}" 
-        if sys.argv[1].lower().startswith('g'):
+        elif sys.argv[1].lower().startswith('g'):
             GMode()
+
+        elif sys.argv[1].lower().startswith('s'):
+            queryGMode()
+
+        else:
+            print("invalid usage")
+            printHelp()
 
     # help menu
     except IndexError:
         print('To be used with a system arg, printing help menu')
         printHelp()
-    
+
 # Fan modes
+
+
 def perfMode():
+    # Performance mode
+    # "\_SB.AMW3.WMAX 0 0x15 {0x01, 0xa1, 0x00, 0x00}"
+    str('echo "\\_SB.AMW3.WMAX 0 0x15 {0x01, 0xa1, 0x00, 0x00}" > /proc/acpi/call')
     notify_send('Activating performance mode')
-    
+
 
 def balancedMode():
-    pass
+    # Balanced mode
+    # "\_SB.AMW3.WMAX 0 0x15 {0x01, 0xa0, 0x00, 0x00}"
+    notify_send('Activating balanced mode')
+
 
 def quietMode():
-    pass
+    # Quiet mode
+    # "\_SB.AMW3.WMAX 0 0x15 {0x01, 0xa3, 0x00, 0x00}"
+    notify_send('Activating quiet mode')
+
 
 def GMode():
-    pass 
+    # Gmode
+    # "\_SB.AMW3.WMAX 0 0x15 {1, 0xab, 0x00, 0x00}"
+    # "\_SB.AMW3.WMAX 0 0x25 {1, 0x01, 0x00, 0x00}"
 
-def executeAcpiCall(command):
-    tmpShell = pexpect.spawn('pkexec bash', encoding='utf-8', logfile=sys.stdout)
+    if (queryGMode()):
+        notify_send('Deactivating GMode\nActivating balanced mode')
+    else:
+        notify_send('Activating GMode')
+
+
+def queryGMode():
+    # "\_SB.AMW3.WMAX 0 0x14 {0x0b, 0x00, 0x00, 0x00}"
+    # 0xab is on, else off
+    prelimChecks()
+    executeAcpiCall(
+        'echo "\\_SB.AMW3.WMAX 0 0x14 {0x0b, 0x00, 0x00, 0x00}" > /proc/acpi/call; cat /proc/acpi/call')
+    if tmpShell.before.find('0xab') > 0:
+        # fan on
+        notify_send('Fan On')
+        return True
+    else:
+        # fan off
+        notify_send('Fan Off')
+        return False
+
+
+# check if acpi call is loaded
+def prelimChecks():
+    elevate()
+    executeAcpiCall('lsmod | grep -i acpi')
+    if tmpShell.before.find('acpi_call') > 0:
+        pass
+    else:
+        notify_send('Acpi Call module not loaded')
+        exit()
+
+# gain root perms
+def elevate():
+    tmpShell.sendline('pkexec bash')
     tmpShell.expect("[#$]")
+
+# execute given acpi call
+def executeAcpiCall(command):
     tmpShell.sendline(command)
     tmpShell.expect("[#$]")
 
+# send notifs using notify_send
 def notify_send(message):
     message = '"' + message + '"'
     notify = pexpect.spawn('bash', encoding='utf-8', logfile=sys.stdout)
@@ -62,10 +114,12 @@ def notify_send(message):
     notify.expect("[#$]")
     notify.close()
 
+
 def printHelp():
     print('Usage:')
     print('python g15-fan-cli.py {{mode}}')
     print('Replace mode with:\nb\tBalanced\np\tPerformance\nq\tQuiet\ng\tGame Shift mode, this is a toggle, will toggle back to balanced if run again\nh\tHelp menu')
+
 
 if __name__ == '__main__':
     parseAgrs()
--- os-shell-test.py
+++ os-shell-test.py
@@ -1,8 +1,18 @@
 import sys
 import pexpect
 
+
+shell = pexpect.spawn('bash', encoding='utf-8')
+shell.expect("[#$]")
+
+output = ''
+
+def execute(command):
+    shell.sendline(command)
+    shell.expect("[#$]")
+    print(shell.before.find('whoami'))
+
+
+
 if __name__ == '__main__':
-    shell = pexpect.spawn('bash', encoding='utf-8', logfile=sys.stdout)
-    shell.expect("[#$]")
-    shell.sendline('whoami')
-    shell.expect("[#$]")
+    execute('whoami')
